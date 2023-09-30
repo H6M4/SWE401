6974a00295ace633289ee422a6cd8e32661f0b46
H6M4 <alsh7ii1@gmail.com>
2023-10-01 00:32:35 +0400

    Submission for SWE401

---
+++ SWE401/src/swe401/SWE401.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package swe401;
+
+/**
+ *
+ * @author Almaq
+ */
+public class SWE401 {
+    public static void main(String[] args) {
+        
+        int H = 1084218;
+        int A = 1080947;
+        System.out.println("Hamad Saeed Almaqdahi, "+H);
+        System.out.println("Ahmed Rashed Alshehhi, "+A);
+    }  
+}
